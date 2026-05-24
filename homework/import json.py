ce51dad7262fe287c8c7f112e974fdb1f78ee2f1
Julian <lam.nt061108@gmail.com>
2026-05-24 17:22:34 -0400

    update task

--- homework/import json.py
+++ homework/import json.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
-
+import uuid
+from datetime import datetime
 current_dir = Path(__file__).parent
 output_file = current_dir / "data.json"
 
@@ -20,16 +21,45 @@ def save_items(items):
 def Main_menu():
     print("1. Add Item")
     print("2. Display Items")
-    print("3. Delete Item")
-    print("4. Exit")
+    print("3. Delete Items")
+    print("4. Update Items")
+    print("5. Exit")
+
+def Update_Item():
+    items = load_items()
+    if not items:
+        print("No items to update.")
+        return
+    Display_Items()
+    name = input("Enter the task name to update: ")
+    for item in items:
+        if item["task name"] == name:
+            item["task name"] = input("New name: ") 
+            item["description"] = input("New description: ") 
+            item["status"] = input("New status: ") 
+            save_items(items)
+            print("Item updated successfully.")
+            return
+    print("Item not found.")
+
+
 
 
 def Add_Item():
     items = load_items()                          # Step 1: read what's already in the file
     new_item = input("Enter the name of the item to add: ")
-    items.append(new_item)                        # Step 2: add the new item to the list
+    new_description = input("Enter the description of the item added: ")
+    status = input("Enter the status of task (todo, in-progress, done): ")
+    items_visual = {}
+    items_visual["uuid"]= str(uuid.uuid1())
+    items_visual["task name"] = new_item
+    items_visual["description"] = new_description
+    items_visual["status"] = status
+    items_visual["createdAt"] = str(datetime.now())
+
+    items.append(items_visual)                  # Step 2: add the new item to the list
     save_items(items)                             # Step 3: save the updated list back to the file
-    print(f'"{new_item}" added successfully.')
+    print(f'"{new_item}: {new_description}" added successfully.')
 
 
 def Display_Items():
@@ -69,6 +99,8 @@ while True:
     elif selection == "3":
         Delete_Items()
     elif selection == "4":
+        Update_Item()
+    elif selection == "5":
         print("Goodbye!")
         break
     else:
